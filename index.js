a0aebb60261826d98256e26443dbb5c1c1dd604b
Harikrishna_Palakila <36328585+harikrishnapalakila@users.noreply.github.com>
2023-02-23 07:58:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get('/will', function (req, res) {
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
 });
-app.listen(process.env.PORT || 8096);
+app.listen(process.env.PORT || 80);
 module.exports = app;
